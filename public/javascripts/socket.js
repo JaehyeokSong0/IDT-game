ff3bc25629dd4ba9b8fea1d99df0120ba7d09510
JaeHyeok Song <72161388+JaehyeokSong0@users.noreply.github.com>
2021-07-18 18:35:58 +0900

    Implement basic CreateRoom function

--- public/javascripts/socket.js
+++ public/javascripts/socket.js
@@ -1,14 +1,29 @@
 const socket = io();
+var id = 'tmp'; // TEST CODE
 
 $('#start_btn').click(() => {
     var id = $('#nickname').val();
     // #중복체크 필요
     $('#game_index').hide();
-    $('body').css('backgroundColor','white');
+    $('body').css('backgroundColor', 'white');
     $('#game_lobby').show();
-    socket.emit('joinLobby', id);
 });
 
-socket.on('joinLobby',(id)=> {
-    console.log(id, 'joined Lobby');
-})
+$('#createRoom_btn').click(() => {
+    $(".modal").fadeIn();
+    $(".modal button").click(() => {
+        $(".modal").fadeOut();
+    });
+});
+
+$('#createRoom button').click(()=> {
+    var roomTitle = $('#createRoom input').val();
+    socket.emit('createRoom', id,roomTitle);
+});
+
+socket.on('createRoom', (id, roomNum,roomTitle) => {
+    console.log(id, 'created ', roomNum);
+    //TEST CODE
+    $('#lobby_table td:eq(0)').html(roomNum);
+    $('#lobby_table td:eq(1)').html(roomTitle);
+});
