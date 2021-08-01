1fcaf9d067084fcc09ee57e8ebcb4c62c52f99de
오윤석 <dhdbstjr98@khu.ac.kr>
2021-08-01 14:09:38 +0900

    mysql 계정 정보 .env에서 관리

--- db/connection.js
+++ db/connection.js
@@ -1,9 +1,9 @@
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'password',
-  database : '*'
+  host     : process.env.MYSQL_HOST,
+  user     : process.env.MYSQL_USER,
+  password : process.env.MYSQL_PASSWORD,
+  database : process.env.MYSQL_DATABASE
 });
 
 connection.connect(function(err) {
