be0aece4a734c08fb02c7b1ca6d4406c5d9cc84e
JaeHyeok Song <72161388+JaehyeokSong0@users.noreply.github.com>
2021-08-16 21:40:10 +0900

    Improve initPlayer

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -16,75 +16,6 @@ socket.on('startGame', (room) => {
     initPlayer();
 });
 
-// const canvas = document.getElementById('game_canvas');
-// canvas.width = canvas.offsetWidth;
-// canvas.height = canvas.offsetHeight;
-// const ctx = canvas.getContext("2d");
-
-// canvas.onclick = function clickField(event) {
-//     const x = event.pageX;
-//     const y = event.pageY;
-//     const clickedXY = field.find((element) => {
-//         return ((element.x < x) && (x < element.x + fieldWidth)) && ((element.y < y) && (y < element.y + fieldHeight));
-//     });
-//     return clickedXY;
-// }
-
-// function getFieldCenter(XY) {
-//     try {
-//         var ret = [XY['x'] + fieldWidth / 2, XY['y'] + fieldHeight / 2];
-//     } catch (error) {
-//         ret = -1;
-//         console.error(error);
-//     }
-//     return ret;
-// }
-
-// function getCharacter(color, _x, _y) {
-//     var circle = {
-//         x: 0,
-//         y: 0,
-//         vx: 1,
-//         vy: 1,
-//         draw: () => {
-//             ctx.beginPath();
-//             ctx.arc(_x, _y, 25, 0, 2 * Math.PI);
-//             ctx.fillStyle = color;
-//             ctx.fill();
-//         }
-//     };
-//     circle.x = _x;
-//     circle.y = _y;
-//     return circle;
-// }
-
-// function drawCharacter(character) {
-//     character.draw();
-// }
-// function initPlayer() {
-//     player1 = new Player(roomInfo[2]);
-//     player1.location = 5;
-//     var p1StartLoc = getFieldCenter(getField(5));
-//     player1.character = getCharacter("magenta",p1StartLoc[0], p1StartLoc[1]);
-//     drawCharacter(player1.character);
-
-//     player2 = new Player(roomInfo[3]);
-//     player2.location = 8;
-//     var p2StartLoc = getFieldCenter(getField(8));
-//     player2.character = getCharacter("cyan", p2StartLoc[0], p2StartLoc[1]);
-//     drawCharacter(player2.character);
-// }
-
-// function getField(fieldNum) {
-//     //Get field info by fieldNum
-//     var XY = field.find((element) => {
-//         return element.fieldNum == fieldNum;
-//     });
-//     return XY;
-// }
-
-// initPlayer();
-
 const canvas = new fabric.Canvas('game_canvas');
 window.addEventListener('resize', resizeCanvas, false);
 
@@ -120,4 +51,38 @@ function drawField(width, height) {
 }
 drawField(fieldWidth, fieldHeight);
 
-console.log("All Objects : ", canvas.getObjects());
+function initPlayer() {
+    player1 = new Player(roomInfo[2]);
+    player1.location = 5;
+    player1.character = setCharacter('magenta', 5);
+    
+    player2 = new Player(roomInfo[3]);
+    player2.location = 8;
+    player2.character = setCharacter('cyan', 8);
+
+    canvas.add(player1.character, player2.character);
+}
+
+function setCharacter(color, location) {
+    var _field = getFieldByNum(location);
+    var _radius = fieldHeight / 6;
+    return new fabric.Circle({
+        radius: _radius,
+        left: _field.left + fieldWidth / 2 - _radius,
+        top: _field.top + fieldHeight / 2 - _radius,
+        fill: color,
+        selectable: false
+    });
+}
+
+function getFieldByNum(fieldNum) {
+    var ret = null;
+    canvas.getObjects().forEach((obj) => {
+        if (obj.fieldNum == fieldNum) {
+            ret = obj;
+        }
+    })
+    return ret;
+}
+
+initPlayer();
