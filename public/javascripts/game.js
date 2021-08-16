12a8afc73dbeaecf4750c116430049d67768e8ab
JaeHyeok Song <72161388+JaehyeokSong0@users.noreply.github.com>
2021-08-17 01:28:02 +0900

    Implement moveChar prototype

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -36,6 +36,7 @@ function drawField(width, height) {
     for (var i = 1; i <= 3; i++) {
         for (var j = 1; j <= 4; j++) {
             canvas.add(new fabric.Rect({
+                objType: 'field',
                 left: j * width,
                 top: i * height,
                 width: width,
@@ -55,7 +56,7 @@ function initPlayer() {
     player1 = new Player(roomInfo[2]);
     player1.location = 5;
     player1.character = setCharacter('magenta', 5);
-    
+
     player2 = new Player(roomInfo[3]);
     player2.location = 8;
     player2.character = setCharacter('cyan', 8);
@@ -67,9 +68,10 @@ function setCharacter(color, location) {
     var _field = getFieldByNum(location);
     var _radius = fieldHeight / 6;
     return new fabric.Circle({
+        objType: 'character',
         radius: _radius,
-        left: _field.left + fieldWidth / 2 - _radius,
-        top: _field.top + fieldHeight / 2 - _radius,
+        left: _field.left + (fieldWidth / 2) - _radius,
+        top: _field.top + (fieldHeight / 2) - _radius,
         fill: color,
         selectable: false
     });
@@ -77,12 +79,44 @@ function setCharacter(color, location) {
 
 function getFieldByNum(fieldNum) {
     var ret = null;
-    canvas.getObjects().forEach((obj) => {
+    canvas.getObjects('rect').forEach((obj) => {
         if (obj.fieldNum == fieldNum) {
             ret = obj;
+            return ret;
         }
     })
     return ret;
 }
 
 initPlayer();
+
+canvas.on('mouse:down', (evt) => moveChar(evt, 12));
+
+// Function to move character by click
+function moveChar(evt, to) {
+    try {
+        if (evt.target.objType == 'character') {
+            var toField = getFieldByNum(to);
+            var dx = toField.left - evt.target.left - evt.target.radius + fieldWidth / 2;
+            var dy = evt.target.top - toField.top + evt.target.radius - fieldHeight / 2;
+            if (dx >= 0) {
+                evt.target.animate('left', '+=' + dx, {
+                    onChange: canvas.renderAll.bind(canvas)
+                });
+            } else {
+                evt.target.animate('left', '-=' + Math.abs(dx), {
+                    onChange: canvas.renderAll.bind(canvas)
+                });
+            }
+            if (dy >= 0) {
+                evt.target.animate('top', '-=' + dy, {
+                    onChange: canvas.renderAll.bind(canvas)
+                });
+            } else {
+                evt.target.animate('top', '+=' + Math.abs(dy), {
+                    onChange: canvas.renderAll.bind(canvas)
+                });
+            }
+        }
+    } catch (error) {}
+}
